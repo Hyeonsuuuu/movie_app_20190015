69cc22e305f01a8cd4c5cb89c61ffa1e0a6b4bde
Hyeonsuuuu <cow549@naver.com>
2022-04-07 10:35:44 +0900

    04-3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({name, picture}) {
+  return (
+    <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vhL7flr5LCRcKWCNr95t3wHaD4%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.UzL1TiB5iNdl90e3Dq_X7AHaD4%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Doncasu',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.DxkwyyLp8y5OyBlvX2xbXwHaEO%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.7qWCjTMnbwtqMGiRErcH3gHaE-%26pid%3DApi&f=1'
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {foodILike.map(dish =>
+        <Food key={dish.name} name={dish.name} picture={dish.image} />
+      )}
+     </div>
   );
 }
 
